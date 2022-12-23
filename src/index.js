9a25fad3ce7237297e6ec088a00c55d7b6864068
gui_kenzo <guilhermekenzo01@gmail.com>
2022-12-23 13:31:50 -0300

    fix: corrigo problemas dos endpoints e adicionado middleware para evitar duplicacao de codigo

--- src/index.js
+++ src/index.js
@@ -6,69 +6,79 @@ const app = express();
 
 app.use(express.json());
 
-const repositories = [];
+const repositories = [
+  {
+    id: "1234",
+    url: "https://github.com/Rocketseat/umbriel",
+    title: "Umbriel",
+    techs: ["Node", "Express", "TypeScript"],
+    likes: 0,
+  },
+];
+
+const getRepository = (request, response, next) => {
+  const { id } = request.params;
+
+  const repository = repositories.find((repo) => repo.id === id);
+
+  if (!repository)
+    return response.status(404).json({ error: "Repository not found!" });
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === repository.id);
+
+  request.repository = repository;
+  request.repoIndex = repoIndex;
+
+  next();
+};
+
+const repositoriesEditableProps = ["url", "title", "techs"];
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
     title,
     url,
     techs,
-    likes: 0
+    likes: 0,
   };
 
-  return response.json(repository);
-});
-
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  repositories.push(repository);
 
   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+app.put("/repositories/:id", getRepository, (request, response) => {
+  const { repository } = request;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  for (let param in request.body) {
+    if (repositoriesEditableProps.includes(param) && param && param.length > 0)
+      repository[param] = request.body[param];
   }
 
-  repositories.splice(repositoryIndex, 1);
+  return response.status(200).json(repository);
+});
+
+app.delete("/repositories/:id", getRepository, (request, response) => {
+  const { repoIndex } = request;
+
+  repositories.splice(repoIndex, 1);
 
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+app.post("/repositories/:id/like", getRepository, (request, response) => {
+  const { repository } = request;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repository.likes += 1;
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
